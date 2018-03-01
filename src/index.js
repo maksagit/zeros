fca400173da8a4d8cda2a547bc92b5dc18c90e5c
maksagit <maksibon@gmail.com>
2018-03-01 22:03:58 +0300

    Factorial zeros count

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  // example: 100! ==> x = 100 / 5 == 20, y = 100 / 25 == 4, ==> 20+4 == 24zero;
+  var zerosCount= 0;
+  var i = 5; 
+
+  for (i; number/i>1; i*=5) {
+    zerosCount+= Math.floor(number/i);
+  }
+ 
+  return zerosCount; 
 }
